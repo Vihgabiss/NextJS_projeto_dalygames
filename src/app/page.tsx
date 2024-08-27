afbf3d6b42d35e97dc4a4a75905a8951705fbb3c
Vitória Santana <vitoriagcsantana@gmail.com>
2024-08-27 13:14:25 -0300

    gerando o jogo do dia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@/components/container";
 import { GameProps } from "@/utils/types/games";
+import next from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { BsArrowRightSquare } from "react-icons/bs";
 
 
 async function getDalyGame() {
   try{
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {cache: 'no-store'});
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {next: {revalidate: 10}});
     return res.json();
   }catch(err){
     throw new Error("Failed to fetch data");
@@ -14,9 +16,7 @@ async function getDalyGame() {
 }
 
 export default async function Home() {
-  const dalyGame: GameProps = await getDalyGame();
-  console.log(dalyGame);
-  
+  const dalyGame: GameProps = await getDalyGame(); 
 
   return (
     <main className="w-full">
@@ -24,13 +24,20 @@ export default async function Home() {
         <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você</h1>
         <Link href={`/game/${dalyGame.id}`}>
         <section className="w-full bg-black rounded-lg">
+          <div className="w-full max-h-96 h-96 relative">
+            <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+              <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+              <BsArrowRightSquare size={24} color="#fff"/>
+            </div>
           <Image 
           src={dalyGame.image_url}
           alt={dalyGame.title}
           priority={true}
           quality={100} 
-          width={100}
-          height={100}/>
+          fill={true}
+          className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"/>
+          </div>
         </section>
         </Link>
       </Container>
